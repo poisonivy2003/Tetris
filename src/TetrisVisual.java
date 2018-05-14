db7f82ee515c7595d4e73764a9b6e49e024d640b
Andrew <andrewchang0228@gmail.com>
2018-05-13 21:52:11 -0700

    Replace TetrisVisual.java with source file from Andrew

--- src/TetrisVisual.java
+++ src/TetrisVisual.java
@@ -1,104 +1,79 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Window;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.*;
 
-public class MyTetris extends JPanel 
-				implements ActionListener{
-	private int time = 0, x = 150, y = 0;
-	public static final int height = 200;
-	public static final int totalRow = height/20;
-	private enum State {READY, MOVING, STOPPED}
-	private State currentState;
+
+
+
+public class TetrisVisual extends JPanel implements ActionListener {
+	private Blocks mBlock;
+	private Grid mG;
+	private int mSquareWidth;
+	private int mSquareHeight;
+	Action mA;
+	public static int time;
 	
-	public MyTetris() 
-	{
-		Timer clock = new Timer(30, this); 
+	public TetrisVisual() {
+		mBlock = Blocks.chooseBlock();
+		mA  = new Action();
+		addKeyListener(mA);
+		mG = new Grid(6, 15);
+		mSquareWidth = 50;
+		mSquareHeight = 50;
+		time = 1000;
+		Timer clock = new Timer(time, this);
 		clock.start();
+
+		// By default JPanel is not mouse-focused
+		// see: https://stackoverflow.com/questions/11487369/jpanel-doesnt-response-to-keylistener-event
+		this.setFocusable(true);
 	}
 	
-	public static void main(String[] args)
+	public void paintComponent (Graphics g)
 	{
-		JFrame window = new JFrame("MineTetris");
-		window.setBounds(100, 100, 300, 200);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JPanel canvas = new MyTetris();
-		canvas.setBackground(Color.WHITE);
-		window.getContentPane().add(canvas);
-		window.setVisible(true);
+		int[][] matrix = mG.getMesh();
+		for (int row=0; row<matrix.length; row++) {
+			for (int column=0; column<matrix[row].length; column++) {
+				int cell = matrix[row][column];
+				int x = column*mSquareWidth;
+				int y = row*mSquareHeight;
+				if (cell == 0) {
+				    g.setColor(Color.GRAY);
+				} else {
+					g.setColor(Color.RED);
+				}
+				g.fillRect(x, y, mSquareWidth-3, mSquareHeight-3);				
+			}
+					
+		}
+//		block.shape(g);
+//		
+//		block.turn(g);
+//		repaint();
+		//System.out.println("hello");
 	}
 	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-	    int width = getWidth();    
-	    int height = getHeight();
-	    int stop, currenty;
-		Blocks chang = new JBlock(x, y);
-		//Blocks chen = new SquareBlock(x, y);
-		//currenty = y;
-		Grid yes = new Grid(chang, 6, 15);
-//		Grid no = new Grid(chen);
-//		stop = yes.stop(15);
-//		if (stop == 0)
-//	    {
-//			Blocks.setYPos(Blocks.getYPos());
-//	    }
-//	    else
-//	    {
-	    	y = (int)(time);
-//	    }
-//		if (time / 50 > 3)
-//		{
-//			chang.turn(g);
-//		}
-//		else
-//		{
-			chang.shape(g);
-//		}
-//		if (currentState == State.READY)
-//		{
-//			chang.shape(g);
-//			currentState = State.MOVING;
-//		}
-//		else if (currentState == State.MOVING)
-//		{
-//			chang.turn(g);
-//		}
-//		else if (currentState == State.STOPPED) // never happens
-//		{
-//			y = currenty;
-//			currentState = State.READY;
-//		}
+	
+	public void actionPerformed(ActionEvent e) 
+	{
+		repaint();
+	}
+	
+	public static void main(String[] args) {
+		JFrame window = new JFrame();
+		TetrisVisual t = new TetrisVisual();
+		window.getContentPane().add(t);
+		window.setSize(50*6, 50*15);
+		window.setVisible(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		
 	}
 
-	public void actionPerformed(ActionEvent a) {
-		time++;
-	    repaint();
-	}
-	
-	private void chooseBlock()
-	{
-		int choose = (int) (Math.random() * 7);
-		switch (choose)
-		{
-		case 1: //Iblock
-			IBlock i = new IBlock(x, 7);
-		case 2: 
-			//
-		case 3:
-			//
-		case 4:
-			//
-		case 5:
-			JBlock j = new JBlock(x, y);
-		case 6:
-			//
-		case 7:
-			//
-		}
-	}
 }
+
+		
